465e63cc0d2c549afae345dd2972c525267e51d3
apstav <apostolis.stav@gmail.com>
2024-06-27 13:17:36 +0300

    v0.8

--- src/main/java/gr/uninystems/rdi/iot_data_aquisition/repository/TrashBinRepository.java
+++
@@ -1,8 +0,0 @@
-//package gr.uninystems.rdi.iot_data_aquisition.repository;
-//
-//import gr.uninystems.rdi.iot_data_aquisition.model.TrashBin;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TrashBinRepository extends JpaRepository<TrashBin, Long> {
-//
-//}
--- src/main/java/gr/uninystems/rdi/iot_data_aquisition/routes/EventRoute.java
+++ src/main/java/gr/uninystems/rdi/iot_data_aquisition/routes/EventRoute.java
@@ -1,104 +1,104 @@
-//package gr.uninystems.rdi.iot_data_aquisition.routes;
-//
-//import gr.uninystems.rdi.iot_data_aquisition.model.Event;
-//import gr.uninystems.rdi.iot_data_aquisition.processors.event.EventProcessor;
-//import gr.uninystems.rdi.iot_data_aquisition.service.EventService;
-//import org.apache.camel.Exchange;
-//import org.apache.camel.builder.RouteBuilder;
-//import org.apache.camel.model.rest.RestParamType;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Component;
-//
-//import static org.apache.camel.language.csimple.CSimpleHelper.bean;
-//
-//@Component
-//public class EventRoute extends RouteBuilder {
-//
-//    //@Value("${routes.iot_data_aquisition.base-rest-path}")
-//    private String baseRestPath = "http://localhost:8080/";
-//
-//    @Autowired
-//    private EventService eventService;
-//
-//    @Autowired
-//    private EventProcessor eventProcessor;
-//
-//    @Override
-//    public void configure() throws Exception {
-//        configureRestEndpoints();
-//        configureDirectRoutes();
-//    }
-//
-//    private void configureRestEndpoints() {
-//        rest(baseRestPath)
-//                .get("/events")
-//                .id("getAllEvents")
-//                .description("Retrieves all events")
-//                .produces("application/json")
-//                .outType(Event[].class)
-//                .to("direct:getAllEvents");
-//
-//        rest(baseRestPath)
-//                .get("/events/{id}")
-//                .id("getEventById")
-//                .description("Retrieves an event by ID")
-//                .produces("application/json")
-//                .outType(Event.class)
-//                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
-//                .to("direct:getEventById");
-//
-//        rest(baseRestPath)
-//                .post("/events")
-//                .id("createEvent")
-//                .description("Creates a new event")
-//                .consumes("application/json")
-//                .produces("application/json")
-//                .type(Event.class)
-//                .outType(Event.class)
-//                .to("direct:createEvent");
-//
-//        rest(baseRestPath)
-//                .put("/events/{id}")
-//                .id("updateEvent")
-//                .description("Updates an existing event")
-//                .consumes("application/json")
-//                .produces("application/json")
-//                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
-//                .param().name("isFree").type(RestParamType.query).dataType("boolean").description("The new value for isFree").endParam()
-//                .to("direct:updateEvent");
-//
-//        rest(baseRestPath)
-//                .delete("/events/{id}")
-//                .id("deleteEvent")
-//                .description("Deletes an event by ID")
-//                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
-//                .to("direct:deleteEvent");
-//    }
-//
-//    private void configureDirectRoutes() {
-//        from("direct:getAllEvents")
-//                .routeId("direct:getAllEvents")
-//                .process(eventProcessor)
-//                .bean(eventService, "findAll");
-//
-//        from("direct:getEventById")
-//                .routeId("direct:getEventById")
-//                .bean(eventService, "findById(${header.id})");
-//
-//        from("direct:createEvent")
-//                .routeId("direct:createEvent")
-//                .bean(eventService, "save")
-//                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.CREATED.value()));
-//
-//        from("direct:updateEvent")
-//                .routeId("direct:updateEvent")
-//                .bean(eventService, "update(${header.id}, ${header.isFree})");
-//
-//        from("direct:deleteEvent")
-//                .routeId("direct:deleteEvent")
-//                .bean(eventService, "deleteBy(${header.id})")
-//                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.NO_CONTENT.value()));
-//    }
-//}
+package gr.uninystems.rdi.iot_data_aquisition.routes;
+
+import gr.uninystems.rdi.iot_data_aquisition.model.Event;
+import gr.uninystems.rdi.iot_data_aquisition.processors.event.EventProcessor;
+import gr.uninystems.rdi.iot_data_aquisition.service.EventService;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import static org.apache.camel.language.csimple.CSimpleHelper.bean;
+
+@Component
+public class EventRoute extends RouteBuilder {
+
+    //@Value("${routes.iot_data_aquisition.base-rest-path}")
+    private String baseRestPath = "http://localhost:8080/";
+
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    private EventProcessor eventProcessor;
+
+    @Override
+    public void configure() throws Exception {
+        configureRestEndpoints();
+        configureDirectRoutes();
+    }
+
+    private void configureRestEndpoints() {
+        rest(baseRestPath)
+                .get("/events")
+                .id("getAllEvents")
+                .description("Retrieves all events")
+                .produces("application/json")
+                .outType(Event[].class)
+                .to("direct:getAllEvents");
+
+        rest(baseRestPath)
+                .get("/events/{id}")
+                .id("getEventById")
+                .description("Retrieves an event by ID")
+                .produces("application/json")
+                .outType(Event.class)
+                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
+                .to("direct:getEventById");
+
+        rest(baseRestPath)
+                .post("/events")
+                .id("createEvent")
+                .description("Creates a new event")
+                .consumes("application/json")
+                .produces("application/json")
+                .type(Event.class)
+                .outType(Event.class)
+                .to("direct:createEvent");
+
+        rest(baseRestPath)
+                .put("/events/{id}")
+                .id("updateEvent")
+                .description("Updates an existing event")
+                .consumes("application/json")
+                .produces("application/json")
+                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
+                .param().name("isFree").type(RestParamType.query).dataType("boolean").description("The new value for isFree").endParam()
+                .to("direct:updateEvent");
+
+        rest(baseRestPath)
+                .delete("/events/{id}")
+                .id("deleteEvent")
+                .description("Deletes an event by ID")
+                .param().name("id").type(RestParamType.path).dataType("long").description("The ID of the event").endParam()
+                .to("direct:deleteEvent");
+    }
+
+    private void configureDirectRoutes() {
+        from("direct:getAllEvents")
+                .routeId("direct:getAllEvents")
+                .process(eventProcessor)
+                .bean(eventService, "findAll");
+
+        from("direct:getEventById")
+                .routeId("direct:getEventById")
+                .bean(eventService, "findById(${header.id})");
+
+        from("direct:createEvent")
+                .routeId("direct:createEvent")
+                .bean(eventService, "save")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.CREATED.value()));
+
+        from("direct:updateEvent")
+                .routeId("direct:updateEvent")
+                .bean(eventService, "update(${header.id}, ${header.isFree})");
+
+        from("direct:deleteEvent")
+                .routeId("direct:deleteEvent")
+                .bean(eventService, "deleteBy(${header.id})")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.NO_CONTENT.value()));
+    }
+}
